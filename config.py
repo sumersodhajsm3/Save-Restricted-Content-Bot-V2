c9b9afeb5286aa9a10d3749853cfd2998a9e63fa
sumersodhajsm3 <sumersingh5688977@gmail.com>
2025-02-07 00:44:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "28662166"))
+API_HASH = getenv("API_HASH", "d03270e7d1eaefee0675a829acd7271c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7548807262:AAEI90v3iONnSKPcGbkkj5kj9zjWQMjEQDk")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7574786767").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002480690858"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
